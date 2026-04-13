edff3fd66f32b96ca674ab8d7400d6a4ec7e250b
ashiqurrhmn <arakash115@gmail.com>
2026-04-13 21:55:34 +0600

    react rounter setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from "react-router";
+import Root from "./layout/Root";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <h2>Home page</h2>,
+      },
+      {
+        path: "/timeline",
+        element: <h2>Timeline page</h2>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h2 className='text-2xl'>Hello, React!</h2>
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
