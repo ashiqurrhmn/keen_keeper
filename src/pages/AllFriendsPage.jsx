f3da13de503808497270b97d7d0c9ab4343d9483
ashiqurrhmn <arakash115@gmail.com>
2026-04-15 20:35:46 +0600

    loading spinner added

--- src/pages/AllFriendsPage.jsx
+++ src/pages/AllFriendsPage.jsx
@@ -1,16 +1,21 @@
-
 import Card from "../componenets/ui/Card";
 
-const AllFriendsPage = ({ friends }) => {
+const AllFriendsPage = ({ friends, loading }) => {
   return (
     <div className="w-9/12 mx-auto">
       <div className="pb-10">
         <h3 className="text-2xl font-semibold">Your Friends</h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-6">
-          {friends.map((friend) => (
-            <Card key={friend.id} friend={friend} />
-          ))}
-        </div>
+        {loading ? (
+          <div className="flex justify-center items-center h-64">
+            <span className="loading loading-spinner loading-xl"></span>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-6">
+            {friends.map((friend) => (
+              <Card key={friend.id} friend={friend} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,21 +5,24 @@ import AllFriendsPage from './AllFriendsPage';
 
 const Homepage = () => {
     const [friends, setFriends] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch('/data.json');
             const data = await res.json();
             setFriends(data);
+            setLoading(false);
         };
         fetchData();
     }, []);
-
+    console.log(loading);
     return (
         <div className='bg-[#F8FAFC]'>
+
             <Banner/>
             <Status friends={friends}/>
-            <AllFriendsPage friends={friends}/>
+            <AllFriendsPage friends={friends} loading={loading} />
         </div>
     );
 };
